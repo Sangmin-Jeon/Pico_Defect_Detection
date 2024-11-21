5857aae18d7373e6524cf1dda423185b9b646473
jeonSangMin <j3k2kk22@gmail.com>
2024-11-22 00:22:23 +0900

    1번 data arguement 작업 완료 나머지 작업 중

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ VISION_API_URL = "https://suite-endpoint-api-apne2.superb-ai.com/endpoints/9a987
 TEAM = "kdt2024_1-9" # 이건 건들 필요 없어요.
 ACCESS_KEY = "Superb AI 사이트에 로그인 해서 넣으세요." # 이건 본인 setting -> ACCESS KEY를 직접 넣어서 사용하세요.
 directory_path = "본인 컴퓨터에 이미지를 저장한 디렉토리 경로 넣으세요."
+store_directory_path = "저장할 경로"
 
 '''
     여기에 필요한 기능을 주석 해제 하여 사용하세요 !!!
@@ -16,12 +17,12 @@ directory_path = "본인 컴퓨터에 이미지를 저장한 디렉토리 경로
 ''' 1. 데이터 증강 시 사용 (학습 데이터 만들때) '''
 # 1번 param에 데이터 증강 시킬 이미지의 경로 넣으세요.
 # 2번 param에 저장할 경로 넣으세요.
-set_data_augment("", "")
+set_data_augment(directory_path, store_directory_path)
 
 ''' 2. gradio 웹 사용 (이미지 직접 업로드)'''
 # start_gradio(VISION_API_URL, TEAM, ACCESS_KEY)
 
-''' 3. 추론 한 이미지 확인 '''
+''' 3. 추론 한 이미지 확인 (아직 작업 중)'''
 # 1번에 directory_path에 추론할 이미지의 경로 넣으세요.
 # show_imaged("", VISION_API_URL, ACCESS_KEY)
 
